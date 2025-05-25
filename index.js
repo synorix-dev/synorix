fcd6ced6322b10abe1ea1ee1341e5f902310ff4e
MrYuzuki933 <mryuzuki933@gmail.com>
2025-05-25 18:20:29 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+function contactMe() {
+  document.getElementById('contactModal').style.display = 'flex';
+}
+
+function closeModal() {
+  document.getElementById('contactModal').style.display = 'none';
+}
